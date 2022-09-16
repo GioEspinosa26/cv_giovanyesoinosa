8392506771e1fa88a99c77407b899a278dba652c
Gio <gespinosaramos683@gmail.com>
2022-09-15 23:12:50 -0500

    Enviar al repository

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+var nombre, edad, ciudad, direccion;
+
+nombre=prompt("Nombre: ","");
+edad=prompt("Edad: ","");
+ciudad=prompt("Ciudad: ","");
+
+
+document.write("Tu nombre es: "+nombre+"<br>"+"Tu edad es: "+edad+" <br>"+"Tu ciudad natal es: "+ciudad+"<br>");
+
